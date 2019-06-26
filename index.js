98067c24cbccd74b11bf1785044815abdbbe3e91
Dan Hintz <dhintz@live.com>
2019-06-26 20:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,14 +52,18 @@ class Trip {
   }
   
   driver() {
-    return this.driver;
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
   }
   
   passenger() {
     return store.passengers.find(
       function(passenger) {
         return passenger.id === this.passengerId;
-      }
-    ).bind(this);
+      }.bind(this)
+    );
   }
 }
