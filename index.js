0d6a257945cc9f1b94b2eb5f58a4e84982f266ca
Dan Hintz <dhintz@live.com>
2019-06-26 20:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let store = {driver: [], passenger: []};
 let driverId = 0;
 class Driver {
   constructor(name) {
-    this.id: ++driverId;
-    this.name: name
-  };
+    this.id = ++driverId;
+    this.name = name;
+  }
 }
